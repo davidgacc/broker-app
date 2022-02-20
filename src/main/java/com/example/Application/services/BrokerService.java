a67d054fdd021bcb621b766cf8427f4f1b815a7b
david arias <david.arias.ext@e-ai.com.br>
2022-02-20 16:27:02 -0300

    Adding logs.

--- src/main/java/com/example/Application/services/BrokerService.java
+++ src/main/java/com/example/Application/services/BrokerService.java
@@ -1,5 +1,6 @@
 package com.example.Application.services;
 
+import com.example.Application.controller.BrokerController;
 import com.example.Application.dao.BrokerDAO;
 import com.example.Application.exceptions.StatusException;
 import com.example.Application.model.Broker;
@@ -7,16 +8,20 @@ import com.example.Application.repository.BrokerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class BrokerService {
 
     private BrokerRepository brokerRepository;
 
+    private static final Logger logger = Logger.getLogger(BrokerController.class.getName());
+
     public Optional<BrokerDAO> getBroker(Long document) throws StatusException {
         Optional<Broker> broker = brokerRepository.findById(document);
         if (broker.isPresent()){
             if (broker.get().getActive()){
+                logger.info("getting data when status is active");
                 BrokerDAO brokerActive =  BrokerDAO.builder()
                         .id(broker.get().getId())
                         .name(broker.get().getName())
